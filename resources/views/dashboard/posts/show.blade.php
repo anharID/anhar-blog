2127b83fded3020042fe9e618d3adf8ddcdbca27
anharID <63109799+anharID@users.noreply.github.com>
2022-03-21 15:27:15 +0700

    edit and delete post

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -6,6 +6,13 @@
         <div class="col-lg-8">
             <h1 class="my-3">{{ $post->title }}</h1>
             <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to all my posts</a>
+            <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><span data-feather="edit"></span> Edit Post</a>
+            <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="d-inline" >
+                @method('DELETE')
+                @csrf
+                <button class="btn btn-danger" onclick="return confirm('Are you sure?')"><span data-feather="x-circle"></span> Delete post</button>
+            </form>
+
             <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid mt-3">
             <article class="my-3 fs-5">
                 {!! $post->body !!}
